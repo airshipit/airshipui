ede25ee6e3fda0920af9963e6558d93c06f69dd8
Gary Smith <garysmith123@gmail.com>
2019-11-18 15:59:28 -0800

    Upgrade to octant 0.9.1
    Change-Id: I890f1b38cbbe394b189358c3c5b1f19a4b96b706

--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.12
 require (
 	github.com/hashicorp/go-plugin v0.0.0-20190220160451-3f118e8ee104
 	github.com/pkg/errors v0.8.1
-	github.com/vmware/octant v0.7.0
-	k8s.io/apimachinery v0.0.0-20190612205821-1799e75a0719
+	github.com/vmware-tanzu/octant v0.9.1
+	k8s.io/apimachinery v0.0.0-20191016225534-b1267f8c42b4
 )
--- internal/plugin/airshipui/register.go
+++ internal/plugin/airshipui/register.go
@@ -6,24 +6,24 @@ SPDX-License-Identifier: Apache-2.0
 package plugin
 
 import (
-    "github.com/vmware/octant/pkg/navigation"
-    "github.com/vmware/octant/pkg/plugin"
-    "github.com/vmware/octant/pkg/plugin/service"
-    "github.com/vmware/octant/pkg/view/component"
+	"github.com/vmware-tanzu/octant/pkg/navigation"
+	"github.com/vmware-tanzu/octant/pkg/plugin"
+	"github.com/vmware-tanzu/octant/pkg/plugin/service"
+	"github.com/vmware-tanzu/octant/pkg/view/component"
 )
 
 func Register(name string, description string) (*service.Plugin, error) {
-    capabilities := &plugin.Capabilities{
-        IsModule: true,
-    }
+	capabilities := &plugin.Capabilities{
+		IsModule: true,
+	}
 
-    // Set up what should happen when Octant calls this plugin.
-    options := []service.PluginOption{
-        service.WithNavigation(handleNavigation, initRoutes),
-    }
+	// Set up what should happen when Octant calls this plugin.
+	options := []service.PluginOption{
+		service.WithNavigation(handleNavigation, initRoutes),
+	}
 
-    // Use the plugin service helper to register this plugin.
-    return service.Register(name, description, capabilities, options...)
+	// Use the plugin service helper to register this plugin.
+	return service.Register(name, description, capabilities, options...)
 }
 
 // handlePrint creates a navigation tree for this plugin. Navigation is dynamic and will
@@ -31,22 +31,22 @@ func Register(name string, description string) (*service.Plugin, error) {
 // The plugin can use whatever paths it likes since these paths can be namespaced to the
 // the plugin.
 func handleNavigation(request *service.NavigationRequest) (navigation.Navigation, error) {
-    return navigation.Navigation{
-        Title: "Airship UI",
-        Path:  request.GeneratePath(),
-        IconName: "cloud",
-    }, nil
+	return navigation.Navigation{
+		Title:    "Airship UI",
+		Path:     request.GeneratePath(),
+		IconName: "cloud",
+	}, nil
 }
 
 // initRoutes routes for this plugin. In this example, there is a global catch all route
 // that will return the content for every single path.
 func initRoutes(router *service.Router) {
-    router.HandleFunc("*", func(request *service.Request) (component.ContentResponse, error) {
-        contentResponse := component.NewContentResponse(component.TitleFromString("Airship UI"))
+	router.HandleFunc("*", func(request *service.Request) (component.ContentResponse, error) {
+		contentResponse := component.NewContentResponse(component.TitleFromString("Airship UI"))
 
-        text := component.NewText("This is the Airship UI plugin.")
-        contentResponse.Add(text)
+		text := component.NewText("This is the Airship UI plugin.")
+		contentResponse.Add(text)
 
-        return *contentResponse, nil
-    })
+		return *contentResponse, nil
+	})
 }
